c63bc19d73531ed0c0461b2898e2ed68f8948285
Max G <max3227@gmail.com>
2020-06-22 02:34:29 +0300

    Regex compilation to improve performance

--- dark-fantasy-2.0.3.py
+++ dark-fantasy-2.0.3.py
@@ -169,9 +169,10 @@ def spider(host):
     print("[*] Use the result to find promising URLs/Emails to try hacking using SQL injection or Xss or Social Engineering etc.\n[*] Depth is the level to go inside the website( usually a small integer ).\n[*] Output will also be saved in text files in the same folder as this software.\n")
     depth = input("Enter the depth level in numbers: ")
     count = 1
+    href_pattern = re.compile('''href=["'](.[^"']+)["']''')
     url = "http://"+host
     with Path(f"depth1.txt").open('w') as out_file:
-        for i in re.findall('''href=["'](.[^"']+)["']''', urllib.request.urlopen(url).read(), re.I):
+        for i in href_pattern.findall(urllib.request.urlopen(url).read(), re.I):
             if "http" not in i:
                 i = "http://"+host+i
             print(i)
@@ -187,7 +188,7 @@ def spider(host):
                     if "http" not in link:
                         link = "http://"+host+link
                     try:
-                        for k in re.findall('''href=["'](.[^"']+)["']''', urllib.request.urlopen(link).read(), re.I):
+                        for k in href_pattern.findall(urllib.request.urlopen(link).read(), re.I):
                             print(k)
                             write_file.write(k+"\n")
                     except:
