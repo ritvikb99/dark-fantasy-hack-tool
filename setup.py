626f4b7c5b3e01b862ebeb133528d4956cac658d
Nelson-Gon <gonzabato@hotmail.com>
2021-03-16 16:28:22 +0800

    make pip friendly, specify html2text version #8

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
-
+# Change version to desired version number
 setup(name='dark-fantasy-hack-tool',
       packages=find_packages(),
-      install_requires=['htm2text'],
+      version="0.1.0",
+      install_requires=['html2text'],
       zip_safe=False)
