2ef3142329e7fc428378073321a968a594d8b606
Ritvik Bhatia <ritvikb99@gmail.com>
2022-08-27 05:00:59 +0530

    Minor changes
    Improved UI 
    User can now select whether to use http or https
    Now writing output into a file

--- modules/subdomain_scanner.py
+++ modules/subdomain_scanner.py
@@ -3,12 +3,16 @@
 '''
 # coding: utf-8
 # Scanner v0.1.0
-# AUTHORS Meuz G - github.com/meuzgebre
+# AUTHORS Meuz G - github.com/meuzgebre 
+# EDITED FOR DARK FANTASY Ritvik Bhatia - github.com/ritvikb99
 '''
 
 # import the required module
 import requests
 from termcolor import colored
+from modules.clear_scr import clear_scr
+from pathlib import Path
+
 
 def get_subdomains(text_file):
     '''
@@ -25,7 +29,7 @@ def get_subdomains(text_file):
             if len(line) == 0:
                 continue
             else:
-                subdomains.append(line)            
+                subdomains.append(line)
 
     # Return the list of subdomains
     return subdomains
@@ -38,32 +42,60 @@ def scan_subdomains(domain_name):
         params: domain_name | str - the domain name to scan for
         return: list of all the subdomains of a given domain name
     '''
+    print(colored(f'[+] Subdomain Scanner', 'green'))
+    try:
+        ssl_enabled = int(
+            input("(SSL)Do you want to check urls with which prefix \n1. http:// \n2. https:// \n"))
+    except (ValueError, EOFError, KeyboardInterrupt):
+        return print('\n[!] Interrupted! or Wrong Value')
+
+    if ssl_enabled == 1:
+        ssl_enabled = False
+    elif ssl_enabled == 2:
+        ssl_enabled = True
+    else:
+        return print("Invalid Choice")
+
+    clear_scr()
 
     # Check doman name pattern matches
-
     sub_domains = get_subdomains('modules/subdomains.txt')
-    subdomains_list = []    # list of all valis subdomains
+    subdomains_list = []    # list of all valid subdomains
     valid_subdomains = 0    # total number of subdomains found
+    url_prefix = "https://" if ssl_enabled else "http://"
 
     # Check if subdomains is valid using requests
+    print(colored('[+] Subdomain Scanner', 'green'))
+    print("[+] Scanning for subdomains")
+    print("[+] Program will return to main menu once scanning is complete")
+    print("[+] You can press Ctrl+C to stop scanning")
+    print("[+] Output Saved in subdomain_output.txt [same folder as software]")
+    print("[+] If you want to use your list of subdomains. Please edit *dark_fantasy_folder*/modules/subdomains.txt\n")
 
-    for sub_domain in sub_domains:
-
-        url = f'https://{sub_domain}.{domain_name}'
-
+    with Path("subdomain_output.txt").open('w') as out_file:
         try:
+            for sub_domain in sub_domains:
 
-            # Sending get request to url
-            requests.get(url)
+                url = f'{url_prefix}{sub_domain}.{domain_name}'
 
-            # if valid print url
-            print (colored(f'[+] {url}', 'blue'))
+                try:
 
-            # add result to list
-            subdomains_list.append(url)
+                    # Sending get request to url
+                    requests.get(url)
 
-            valid_subdomains += 1
-        except requests.ConnectionError:
-            pass
+                    # if valid print url
+                    print(colored(f'[+] {url}', 'blue'))
+
+                    # add result to list
+                    subdomains_list.append(url)
+
+                    # add url to output file
+                    out_file.write(url+'\n')
+
+                    valid_subdomains += 1
+                except requests.ConnectionError:
+                    pass
+        except KeyboardInterrupt:
+            return print("You pressed Ctrl+C")
 
     return subdomains_list
