01e326608e24798069a4fc2cd8ae4dd5fa6d2d9b
Meuz G <95613515+meuzgebre@users.noreply.github.com>
2022-08-26 05:21:38 -0800

    Updating dark-fantasy.py
    Updating dark-fantasy.py file to make use of the added custom subdomain_scanner module.

--- dark-fantasy.py
+++ dark-fantasy.py
@@ -5,6 +5,7 @@ from modules.ftp import ftp
 from modules.banner import banner
 from modules.spider import spider
 from modules.email import email
+from modules.subdomain_scanner import scan_subdomains
 
 
 def ask_host():
@@ -20,28 +21,31 @@ def main():
         print("-"*60+"\n")
         print("                  Dark Fantasy - Hack Tool                    ")
         print("-"*60+"\n")
-        print("1.Port Scanning\n2.DDOS\n3.Banner Grabbing\n4.Web spider(gather all URLs for web hacking)\n5.FTP Password Cracker\n6.Email Scraping")
+        print("1.Port Scanning\n2.DDOS\n3.Banner Grabbing\n4.Web spider(gather all URLs for web hacking)\n5.FTP Password Cracker\n6.Email Scraping\n7.Subdomain Scanner")
         try:
             choice = int(input("Enter Your Choice: "))
         except (ValueError, EOFError, KeyboardInterrupt):
             return print('\n[!] Interrupted! or Wrong Value')
 
-        if choice not in range(7):
+        if choice not in range(8):
             return print('Invalid choice')
 
         hostname = ask_host()
         if choice == 1:
             scanner(hostname)
-        elif choice == 6:
-            email(hostname)
-        elif choice == 3:
-            banner(hostname)
-        elif choice == 5:
-            ftp(hostname)
         elif choice == 2:
             dos(hostname)
+        elif choice == 3:
+            banner(hostname)
         elif choice == 4:
             spider(hostname)
+        elif choice == 5:
+            ftp(hostname)
+        elif choice == 6:
+            email(hostname)
+        elif choice == 7:
+            # Only accepts domain name such as example.com, ip address aren't valid input
+            scan_subdomains(hostname)
         else:
             print("Invalid choice")
 
