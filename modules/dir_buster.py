a24c9c259ba2ea5869bed0712e426639ea5bee72
Ritvik Bhatia <ritvikb99@gmail.com>
2022-09-03 08:07:48 +0530

    Update dir_buster.py

--- modules/dir_buster.py
+++ modules/dir_buster.py
@@ -4,11 +4,18 @@
 # coding: utf-8
 # Scanner v0.1.0
 # AUTHORS Meuz G - github.com/meuzgebre
+# EDITED FOR DARK FANTASY Ritvik Bhatia - github.com/ritvikb99
 '''
 
 # import the required module
+from pathlib import Path
+from termcolor import colored
 import requests
+from modules.clear_scr import clear_scr
+
+
 # from termcolor import colored
+
 
 def get_wordlist(text_file):
     '''
@@ -25,7 +32,7 @@ def get_wordlist(text_file):
             if len(line) == 0:
                 continue
             else:
-                names.append(line)            
+                names.append(line)
 
     # Return the list of names
     return names
@@ -38,6 +45,28 @@ def scan_urls(domain_name):
         return: list of all the valid urls
     '''
 
+    try:
+        ssl_enabled = int(
+            input("(SSL)Do you want to check urls with which prefix \n1. http:// \n2. https:// \n"))
+    except (ValueError, EOFError, KeyboardInterrupt):
+        return print('\n[!] Interrupted! or Wrong Value')
+
+    if ssl_enabled == 1:
+        ssl_enabled = False
+    elif ssl_enabled == 2:
+        ssl_enabled = True
+    else:
+        return print("Invalid Choice")
+    clear_scr()
+
+    url_prefix = "https://" if ssl_enabled else "http://"
+
+    print("[+] Program will return to main menu once bruteforcing is complete")
+    print("[+] You can press Ctrl+C to stop scanning")
+    print("[+] Output Saved in dirbuster_output.txt [same folder as software]")
+    print("[+] If you want to use your list of directories. Please edit *dark_fantasy_folder*/modules/wordlist.txt\n")
+    print('[+] Brutforcing Started')
+
     # Check doman name pattern matches
 
     paths = get_wordlist('modules/wordlist.txt')
@@ -46,25 +75,31 @@ def scan_urls(domain_name):
 
     # Check if url is valid using requests
 
-    for path in paths:
-
-        url = f'https://{domain_name}/{path}'
-
+    with Path("dirbuster_output.txt").open('w') as out_file:
         try:
+            for path in paths:
 
-            print ('############################## Brutforcing Started ##############################')
-            # Sending get request to url
-            response = requests.get(url)
+                url = f'{url_prefix}{domain_name}/{path}'
 
-            # if valid print url
-            if response.status_code == 200:
-                print (f'[+] {url}')
+                try:
 
-                # add result to list
-                url_list.append(url)
+                    # Sending get request to url
+                    response = requests.get(url)
 
-                valid_url += 1
-        except requests.ConnectionError:
-            pass
+                    # if valid print url
+                    if response.status_code == 200:
+                        print(colored(f'[+] {url}', 'blue'))
+
+                        # add result to list
+                        url_list.append(url)
+
+                        # add url to output file
+                        out_file.write(url+'\n')
+
+                        valid_url += 1
+                except requests.ConnectionError:
+                    pass
+        except KeyboardInterrupt:
+            return print("You pressed Ctrl+C")
 
     return url_list
